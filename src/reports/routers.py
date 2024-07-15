805d71d4df641f3eb3248e5a5a3c8ccc34ca8d71
Igor Chesnykh (notebook) <igor.chesnyx@mail.ru>
2024-07-15 21:04:16 +0300

    Prepare to edit database models

--- src/reports/routers.py
+++ src/reports/routers.py
@@ -1,6 +1,6 @@
 from typing import Annotated, List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 from src.reports.models import Report
 from src.reports.schemas import ReportCreate, ReportResponse
@@ -8,10 +8,10 @@ from src.reports.services import ReportService
 from src.users.models import User
 from src.users.services import UserService
 
-router = APIRouter(tags=["fishing"], prefix="/fishing")
+router = APIRouter(tags=["fishing"], prefix="/reports")
 
 
-@router.post("/reports/new")
+@router.post("/new")
 async def create_report(
         report_create: ReportCreate,
         current_user: Annotated[User, Depends(UserService().get_current_user)]
@@ -20,7 +20,15 @@ async def create_report(
     return {"success": "ok"}
 
 
-@router.get("/reports/all", response_model=List[ReportResponse])
+@router.get("/all", response_model=List[ReportResponse])
 async def get_all_reports():
     reports = await ReportService().get_all_reports()
     return reports
+
+
+@router.get("/{report_id}")
+async def get_report_by_id(report_id: int):
+    report = await ReportService().get_report_by_id(report_id)
+    if report is None:
+        raise HTTPException(status_code=404, detail="Report not found")
+    return report
