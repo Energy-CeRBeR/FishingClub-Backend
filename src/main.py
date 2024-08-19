fbcf537c58d61824b707c52a5d6df8ffb255053d
Igor Chesnykh (notebook) <igor.chesnyx@mail.ru>
2024-08-19 14:57:15 +0300

    Start to create admin endpoints

--- src/main.py
+++ src/main.py
@@ -3,13 +3,15 @@ from fastapi import FastAPI
 
 from src.users.routers import router as users_router
 from src.reports.routers import router as reports_router
+from src.admin.routers import router as admin_router
 
 app = FastAPI(
-    title="FishingClub api"
+    title="FishingClub API"
 )
 
 app.include_router(users_router)
 app.include_router(reports_router)
+app.include_router(admin_router)
 
 if __name__ == "__main__":
     uvicorn.run(
